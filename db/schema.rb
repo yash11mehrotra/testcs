fcc16ac431624f262560923a91ab8e96eadcc528
yash <yash11mehrotra@gmail.com>
2014-08-17 23:39:43 +0530

    added unmigrated migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140817165701) do
+ActiveRecord::Schema.define(version: 20140817180450) do
 
   create_table "users", force: true do |t|
     t.string   "name"
