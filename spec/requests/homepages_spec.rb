5d1a1afe422b07cc858a614f0fc32227e65a4412
yash <yash11mehrotra@gmail.com>
2014-08-17 20:12:26 +0530

    first commit

---
+++ spec/requests/homepages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Homepages" do
+  describe "GET /homepages" do
+    it "should have the content 'Master'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit root_path
+      expect(page).to have_content('Master')
+    end
+  end
+end
+
+   
